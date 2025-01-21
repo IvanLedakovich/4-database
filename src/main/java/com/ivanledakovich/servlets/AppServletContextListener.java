5fe6140ca2ead2677ca001354d7dfc9955d1c74d
Ivan Ledakovich <ivanled2002@gmail.com>
2025-01-21 22:20:56 +0100

    Improve configuration properties loading
    Fixes issue:
    #4 - https://github.com/IvanLedakovich/4-database/issues/4
    
    Improves configuration properties loading

--- src/main/java/com/ivanledakovich/servlets/AppServletContextListener.java
+++ src/main/java/com/ivanledakovich/servlets/AppServletContextListener.java
@@ -1,33 +1,50 @@
 package com.ivanledakovich.servlets;
 
+import org.apache.log4j.Logger;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 /**
  * This class contains context logic
  *
- * @author Ivan Ledakovich
+ * Loads configuration from config.txt, with fallback to defaultConfig.txt.
  */
 public class AppServletContextListener implements ServletContextListener {
-    private static Properties properties = new Properties();
 
-    static {
-        try {
-            InputStream is = AppServletContextListener.class.getClassLoader().getResourceAsStream(".env");
-            properties.load(is);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    private static final Logger logger = Logger.getLogger(AppServletContextListener.class);
 
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
     }
 
     @Override
-    public void contextDestroyed(ServletContextEvent servletContextEvent) {}
-
-    @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
+        Properties properties = new Properties();
+        try (InputStream configStream = getResourceAsStream("config.txt")) {
+            if (configStream != null) {
+                properties.load(configStream);
+            } else {
+                try (InputStream defaultStream = getResourceAsStream("defaultConfig.txt")) {
+                    if (defaultStream != null) {
+                        properties.load(defaultStream);
+                    } else {
+                        throw new IllegalArgumentException("Neither config.txt nor defaultConfig.txt found");
+                    }
+                }
+            }
+        } catch (IOException e) {
+            logger.error("Error loading configuration files", e);
+        } catch (IllegalArgumentException e) {
+            logger.error(e.getMessage(), e);
+        }
         servletContextEvent.getServletContext().setAttribute("myEnvironment", properties);
+    }
+
+    private InputStream getResourceAsStream(String resourceName) {
+        return AppServletContextListener.class.getClassLoader().getResourceAsStream(resourceName);
     }
 }
