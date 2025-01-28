16e858679286c175339e8679603c93a597c049ba
Ivan Ledakovich <ivanled2002@gmail.com>
2025-01-28 19:42:19 +0100

    Improve database connection logic
    Fixes issue:
    #6 - https://github.com/IvanLedakovich/4-database/issues/6
    
    Improved database connection logic

--- src/main/java/com/ivanledakovich/servlets/AppServletContextListener.java
+++ src/main/java/com/ivanledakovich/servlets/AppServletContextListener.java
@@ -1,11 +1,9 @@
 package com.ivanledakovich.servlets;
 
-import org.apache.log4j.Logger;
+import com.ivanledakovich.utils.ConfigurationVariables;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.Properties;
 
 /**
@@ -15,32 +13,9 @@ import java.util.Properties;
  */
 public class AppServletContextListener implements ServletContextListener {
 
-    private static final Logger logger = Logger.getLogger(AppServletContextListener.class);
-
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
-        Properties properties = new Properties();
-        try (InputStream configStream = getResourceAsStream("config.properties")) {
-            if (configStream != null) {
-                properties.load(configStream);
-            } else {
-                try (InputStream defaultStream = getResourceAsStream("defaultConfig.properties")) {
-                    if (defaultStream != null) {
-                        properties.load(defaultStream);
-                    } else {
-                        throw new IllegalArgumentException("Neither config.properties nor defaultConfig.properties found");
-                    }
-                }
-            }
-        } catch (IOException e) {
-            logger.error("Error loading configuration files", e);
-        } catch (IllegalArgumentException e) {
-            logger.error(e.getMessage(), e);
-        }
+        Properties properties = ConfigurationVariables.getConfigProperties();
         servletContextEvent.getServletContext().setAttribute("myEnvironment", properties);
-    }
-
-    private InputStream getResourceAsStream(String resourceName) {
-        return AppServletContextListener.class.getClassLoader().getResourceAsStream(resourceName);
     }
 }
--- src/main/java/com/ivanledakovich/utils/ConfigurationVariables.java
+++ src/main/java/com/ivanledakovich/utils/ConfigurationVariables.java
@@ -1,8 +1,15 @@
 package com.ivanledakovich.utils;
 
 import com.ivanledakovich.models.DatabaseDao;
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class ConfigurationVariables {
+
+    private static final Logger logger = Logger.getLogger(ConfigurationVariables.class);
 
     public static DatabaseDao getEnvironmentVariables() {
         DatabaseDao databaseDao = new DatabaseDao();
@@ -11,5 +18,34 @@ public class ConfigurationVariables {
         databaseDao.setPassword(System.getenv().get("DB_PASSWORD"));
         databaseDao.setDriver(System.getenv().get("DB_DRIVER"));
         return databaseDao;
+    }
+
+    public static Properties getConfigProperties() {
+        Properties properties = new Properties();
+        try (InputStream configStream = getResourceAsStream("/config.properties")) {
+            if (configStream != null) {
+                logger.info("Loading config.properties...");
+                properties.load(configStream);
+            } else {
+                logger.warn("config.properties not found. Trying defaultConfig.properties...");
+                try (InputStream defaultStream = getResourceAsStream("/defaultConfig.properties")) {
+                    if (defaultStream != null) {
+                        logger.info("Loading defaultConfig.properties...");
+                        properties.load(defaultStream);
+                    } else {
+                        throw new IllegalArgumentException("Neither config.properties nor defaultConfig.properties found in classpath");
+                    }
+                }
+            }
+        } catch (IOException e) {
+            logger.error("Error loading configuration files", e);
+        } catch (IllegalArgumentException e) {
+            logger.error(e.getMessage(), e);
+        }
+        return properties;
+    }
+
+    private static InputStream getResourceAsStream(String resourceName) {
+        return ConfigurationVariables.class.getResourceAsStream(resourceName);
     }
 }
